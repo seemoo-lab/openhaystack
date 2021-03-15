76a01c187bcb4d8461604b60241a1970af99fb6f
Milan Stute <mstute@seemoo.tu-darmstadt.de>
2021-03-15 18:49:53 +0100

    Add history view (shows all location reports for a single accessory)

--- OpenHaystack/OpenHaystack/HaystackApp/Views/AccessoryMapAnnotation.swift
+++ OpenHaystack/OpenHaystack/HaystackApp/Views/AccessoryMapAnnotation.swift
@@ -103,3 +103,11 @@ class AccessoryAnnotation: NSObject, MKAnnotation {
         self.accessory = accessory
     }
 }
+
+class AccessoryHistoryAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+
+    init(coordinate: CLLocationCoordinate2D) {
+        self.coordinate = coordinate
+    }
+}
