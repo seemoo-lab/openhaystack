923db582e83ed8ded0d897fff45fff3d8e985570
Andrea Baccega <vekexasia@gmail.com>
2022-08-31 15:04:51 +0200

    added list of reports with map interaction and colors for start +end

--- openhaystack-mobile/lib/history/accessory_history.dart
+++ openhaystack-mobile/lib/history/accessory_history.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:openhaystack_mobile/accessory/accessory_model.dart';
@@ -23,16 +24,26 @@ class AccessoryHistory extends StatefulWidget {
 class _AccessoryHistoryState extends State<AccessoryHistory> {
 
   final MapController _mapController = MapController();
+  final ScrollController _scrollController = new ScrollController();
 
   bool showPopup = false;
   Pair<LatLng, DateTime>? popupEntry;
 
   double numberOfDays = 7;
+  int scrolledMarker = -1;
 
   @override
   void initState() {
     super.initState();
-
+    _scrollController.addListener(() {
+      setState(() {
+        var old = scrolledMarker;
+        scrolledMarker = _scrollController.offset.toInt() ~/ 24;
+        if (old != scrolledMarker) {
+          _mapController.move(widget.accessory.locationHistory.elementAt(scrolledMarker).a, _mapController.zoom);
+        }
+      });
+    });
     _mapController.onReady
       .then((_) {
         var historicLocations = widget.accessory.locationHistory
@@ -42,6 +53,55 @@ class _AccessoryHistoryState extends State<AccessoryHistory> {
       });
   }
 
+  List<Marker> buildMarkers() {
+    List<Marker> toRet = [];
+    var length = widget.accessory.locationHistory.length;
+    for (int i=1; i< length-1; i++) {
+      toRet.add(buildMarker(i));
+    }
+    if (length > 0) {
+      toRet.add(buildMarker(0));
+    }
+    if (length > 1) {
+      toRet.add(buildMarker(length-1));
+    }
+    return toRet;
+  }
+
+  Marker buildMarker(int index) {
+    var entry = widget.accessory.locationHistory.elementAt(index);
+    return Marker(
+      point: entry.a,
+
+      builder: (ctx) => GestureDetector(
+        onTap: () {
+          setState(() {
+            showPopup = true;
+            popupEntry = entry;
+          });
+        },
+        child: Icon(
+          Icons.circle,
+          size: (widget.accessory.locationHistory.first == entry || widget.accessory.locationHistory.last == entry) ? 20: 10,
+          color: (() {
+            if (widget.accessory.locationHistory.first == entry) {
+              return Colors.amber;
+            }
+            if (widget.accessory.locationHistory.last == entry) {
+              return Colors.green;
+            }
+            if (index == scrolledMarker) {
+              return Colors.pink;
+            }
+            if (entry == popupEntry) {
+              return Colors.red;
+            }
+            return Theme.of(context).indicatorColor;
+          })().withOpacity(0.9),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     // Filter for the locations after the specified cutoff date (now - number of days)
@@ -58,104 +118,112 @@ class _AccessoryHistoryState extends State<AccessoryHistory> {
         title: Text(widget.accessory.name),
       ),
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              flex: 3,
-              fit: FlexFit.tight,
-              child: FlutterMap(
-                mapController: _mapController,
-                options: MapOptions(
-                  center: LatLng(49.874739, 8.656280),
-                  zoom: 13.0,
-                  interactiveFlags:
-                    InteractiveFlag.pinchZoom | InteractiveFlag.drag |
-                    InteractiveFlag.doubleTapZoom | InteractiveFlag.flingAnimation |
-                    InteractiveFlag.pinchMove,
-                  onTap: (_, __) {
+        child: Scrollbar(
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 3,
+                fit: FlexFit.tight,
+                child: FlutterMap(
+                  mapController: _mapController,
+                  options: MapOptions(
+                    center: LatLng(49.874739, 8.656280),
+                    zoom: 13.0,
+                    maxZoom: 18.25,
+                    interactiveFlags:
+                      InteractiveFlag.pinchZoom | InteractiveFlag.drag |
+                      InteractiveFlag.doubleTapZoom | InteractiveFlag.flingAnimation |
+                      InteractiveFlag.pinchMove,
+                    onTap: (_, __) {
+                      setState(() {
+                        showPopup = false;
+                        popupEntry = null;
+                      });
+                    },
+                  ),
+                  layers: [
+                    TileLayerOptions(
+                      backgroundColor: Theme.of(context).colorScheme.surface,
+                      tileBuilder: (context, child, tile) {
+                        var isDark = (Theme.of(context).brightness == Brightness.dark);
+                        return isDark ? ColorFiltered(
+                          colorFilter: const ColorFilter.matrix([
+                            -1, 0, 0, 0, 255,
+                            0, -1, 0, 0, 255,
+                            0, 0, -1, 0, 255,
+                            0, 0, 0, 1, 0,
+                          ]),
+                          child: child,
+                        ) : child;
+                      },
+                      // urlTemplate: "https://mt0.google.com/vt/lyrs=m@221097413&x={x}&y={y}&z={z}",
+                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c'],
+                      attributionBuilder: (_) {
+                        return const Text("© OpenStreetMap contributors");
+                      },
+                    ),
+                    // The line connecting the locations chronologically
+                    PolylineLayerOptions(
+                      polylines: [
+                        Polyline(
+                          points: locationHistory.map((entry) => entry.a).toList(),
+                          strokeWidth: 4,
+                          color: Theme.of(context).colorScheme.primaryVariant,
+                          isDotted: true
+                        ),
+                      ],
+                    ),
+                    // The markers for the historic locaitons
+                    MarkerLayerOptions(
+                      markers: buildMarkers(),
+                    ),
+                    // Displays the tooltip if active
+                    MarkerLayerOptions(
+                      markers: [
+                        if (showPopup) LocationPopup(
+                          location: popupEntry!.a,
+                          time: popupEntry!.b,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: DaysSelectionSlider(
+                  numberOfDays: numberOfDays,
+                  onChanged: (double newValue) {
                     setState(() {
-                      showPopup = false;
-                      popupEntry = null;
+                      numberOfDays = newValue;
                     });
                   },
                 ),
-                layers: [
-                  TileLayerOptions(
-                    backgroundColor: Theme.of(context).colorScheme.surface,
-                    tileBuilder: (context, child, tile) {
-                      var isDark = (Theme.of(context).brightness == Brightness.dark);
-                      return isDark ? ColorFiltered(
-                        colorFilter: const ColorFilter.matrix([
-                          -1, 0, 0, 0, 255,
-                          0, -1, 0, 0, 255,
-                          0, 0, -1, 0, 255,
-                          0, 0, 0, 1, 0,
-                        ]),
-                        child: child,
-                      ) : child;
-                    },
-                    urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    subdomains: ['a', 'b', 'c'],
-                    attributionBuilder: (_) {
-                      return const Text("© OpenStreetMap contributors");
-                    },
-                  ),
-                  // The line connecting the locations chronologically
-                  PolylineLayerOptions(
-                    polylines: [
-                      Polyline(
-                        points: locationHistory.map((entry) => entry.a).toList(),
-                        strokeWidth: 4,
-                        color: Theme.of(context).colorScheme.primaryVariant,
-                      ),
-                    ],
-                  ),
-                  // The markers for the historic locaitons
-                  MarkerLayerOptions(
-                    markers: locationHistory.map((entry) => Marker(
-                      point: entry.a,
-                      builder: (ctx) => GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            showPopup = true;
-                            popupEntry = entry;
-                          });
-                        },
-                        child: Icon(
-                          Icons.circle,
-                          size: 15,
-                          color: entry == popupEntry
-                            ? Colors.red
-                            : Theme.of(context).indicatorColor,
-                        ),
-                      ),
-                    )).toList(),
-                  ),
-                  // Displays the tooltip if active
-                  MarkerLayerOptions(
-                    markers: [
-                      if (showPopup) LocationPopup(
-                        location: popupEntry!.a,
-                        time: popupEntry!.b,
-                      ),
-                    ],
-                  ),
-                ],
               ),
-            ),
-            Flexible(
-              flex: 1,
-              fit: FlexFit.tight,
-              child: DaysSelectionSlider(
-                numberOfDays: numberOfDays,
-                onChanged: (double newValue) {
-                  setState(() {
-                    numberOfDays = newValue;
-                  });
-                },
-              ),
-            ),
-          ],
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: ScrollConfiguration(
+                  behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {
+                    PointerDeviceKind.touch,
+                    PointerDeviceKind.mouse,
+                  },),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8.0),
+                    child: ListView(
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      padding: EdgeInsets.only(top:12),
+                      controller: _scrollController,
+                      scrollDirection: Axis.vertical,
+                      children: locationHistory.map((l) => SizedBox(height: 24, child: Text(l.b.toString()))).toList(),
+                    ),
+                  ),
+                )
+              )
+            ],
+          ),
         ),
       ),
     );
